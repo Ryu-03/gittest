ad9a08549d561de34e0d25db14b7457ff2c46363
Ryu-03 <sato.ryuhei03@gmail.com>
2020-06-24 14:48:30 +0900

    2行に分けました。

--- GitTest/src/test1/BookMain.java
+++ GitTest/src/test1/BookMain.java
@@ -8,7 +8,8 @@ public class BookMain {
 		book.setTitle("Train Collection");
 		book.setPage(250);
 		
-		System.out.println("Title:" + book.getTitle() + " , " +  "Page:" + book.getPage());
+		System.out.println("Title: " + book.getTitle());
+		System.out.println("Page: " + book.getPage());
 	}
 
 }
