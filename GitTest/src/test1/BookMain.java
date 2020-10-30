293882e36e576ff388a34ae7c4bbf6e2e612c291
Ryuhei Sato <million.ways.neuerer@gmail.com>
2020-10-30 16:04:54 +0900

    修正４

--- GitTest/src/test1/BookMain.java
+++ GitTest/src/test1/BookMain.java
@@ -6,12 +6,11 @@ public class BookMain {
 		Book book = new Book();
 		
 		book.setTitle("Japanese Recipe");
-		book.setPage(500);
+		book.setPage(900);
 		
 		System.out.println("Title: " + book.getTitle());
 		System.out.println("Page: " + book.getPage());
-		System.out.println("20201026");
-		System.out.println("1400円くらいです");
+		System.out.println("20201030");
 	}
 
 }
