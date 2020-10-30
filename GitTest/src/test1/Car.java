84ae52a2a209fdcc8716066b54823a11d89470ee
Ryuhei Sato <million.ways.neuerer@gmail.com>
2020-10-30 15:46:42 +0900

    add file

---
+++ GitTest/src/test1/Car.java
@@ -0,0 +1,24 @@
+package test1;
+
+public class Car{
+
+	private String name;
+	private int price;
+
+	public String getName(){
+		return name;
+	}
+
+	public int getPrice(){
+		return price;
+	}
+
+	public void setName(String name){
+		this.name = name;
+	}
+	
+	public void setPrice(int price){
+		this.price = price;
+	}
+
+}
